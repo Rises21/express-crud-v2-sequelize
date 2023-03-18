a1c8d5278e4ffcf422eec16e1ee5478b107b12fc
Rises21 <bangkitsetio21@gmail.com>
2023-03-19 01:24:55 +0700

    create simple routing using express-ejs-layout

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const ejsLayout = require('express-ejs-layouts');
+const app = express();
+const port = 3000;
+
+
+app.set('view engine', 'ejs');//use ejs
+app.use(ejsLayout);//use express-ejs-layouts
+
+
+app.get('/', (req, res) => {
+	//res.sendFile('./html/index.html', { root: __dirname });
+	res.render('index', {
+		layout: './layout/layout',
+		pageTitle: 'Homepage'
+	})
+});
+
+app.get('/about', (req, res) => {
+	//res.sendFile('./html/about.html', { root: __dirname });
+	res.render('about', {
+		layout: './layout/layout',
+		pageTitle: 'About Page'
+	})
+});
+
+app.get('/contact', (req, res) => {
+	//res.sendFile('./html/contact.html', { root: __dirname }); //root saat ini adalah folder expressjs
+	res.render('contact', {
+		layout: './layout/layout',
+		pageTitle: 'Contact Page'
+	})
+});
+
+
+
+app.use('/', (req, res) => {
+	res.status(404);
+	res.send('Error 404 !');
+})
+
+app.listen(port, () => {
+	console.log(`Server Express Running at http://localhost:${port}`);
+});
