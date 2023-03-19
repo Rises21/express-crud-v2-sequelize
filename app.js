1dec97b4620317e801d7bbb3a1521e3d741d497e
Rises21 <bangkitsetio21@gmail.com>
2023-03-19 22:12:16 +0700

    edit ejs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.set('view engine', 'ejs');//use ejs
+app.set('view engine',ejs);//use ejs
 app.use(ejsLayout);//use express-ejs-layouts
 
 
