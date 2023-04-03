9845a24f44d5ddcff7e4907178cb361b80488184
Rises21 <bangkitsetio21@gmail.com>
2023-04-03 22:59:20 +0700

    try to deploy using freesqldb

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.use('/', (req, res, next) => {
 	res.json({
 		message: "Please change the route path to https://express-crud-v2-sequelize.onrender.com/api/v2/product"
 	});
-	next();
+	next('route');
 })
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/v1', productRouter);
